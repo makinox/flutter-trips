e36fcf829c551290e6885e808c96bd764119a3ac
Jesus bossa <x@jesusbossa.com>
2019-12-26 18:32:38 -0500

    [IMPROVEMENT] Transparent navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:trips/trips.dart';
+import 'package:trips/navigation_bar.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Trips(),
+      home: Navigation(),
     );
   }
 }
---
+++ lib/navigation_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:trips/trips.dart';
+
+class Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      child: Trips(),
+    );
+  }
+  
+}

8d31ea737fdc213a0b1466472d1b1d3e4ec608e2
Jesus bossa <x@jesusbossa.com>
2020-01-09 15:38:12 -0500

    [ADD] Firebase login rquest

---
+++ lib/User/repository/firebase_auth_api.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthAPI {
+
+  final _auth = FirebaseAuth.instance;
+  final googleSignIn = GoogleSignIn();
+
+  Future<FirebaseUser> singnIn() async{
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+
+    FirebaseUser user = await _auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(idToken: gSA.idToken, accessToken: gSA.accessToken)
+    );
+
+    return user;
+  }
+
+}

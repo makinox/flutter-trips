9b1379c7a2497d8c674e4643c61ede542f749865
Jesus bossa <x@jesusbossa.com>
2020-01-13 21:41:03 -0500

    [ADD] Inputs implemented

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:trips/widgets/gradientBack.dart';
+import 'package:trips/widgets/text_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
 
@@ -18,24 +19,46 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreen extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
+
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
     
     return Scaffold(body: Stack(children: <Widget>[
-      GradientBack('Add a new place', 300),
-      Row(children: <Widget>[
-        Container(
-          padding: EdgeInsets.only(top: 12, left: 0),
-          child: SizedBox(
-            height: 45,
-            width: 45,
-            child: IconButton(
-              icon: Icon(Icons.keyboard_arrow_left, color: Colors.white, size: 45,),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+      GradientBack('Add a new place', 250),
+      Container(
+        padding: EdgeInsets.only(top: 6, left: 0),
+        child: SizedBox(
+          height: 45,
+          width: 45,
+          child: IconButton(
+            icon: Icon(Icons.keyboard_arrow_left, color: Colors.white, size: 45,),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 350, bottom: 20),
+        child: ListView(children: <Widget>[
+          Container(),
+          Container(
+            margin: EdgeInsets.only(bottom: 20),
+            child: TextInput(
+              hintText: 'Title',
+              inputType: null,
+              maxLines: 1,
+              controller: _controllerTitlePlace,
             ),
           ),
-        )
-      ],)
+          TextInput(
+            hintText: 'Decription',
+            inputType: TextInputType.multiline,
+            maxLines: 4,
+            controller: _controllerDescriptionPlace,
+          )
+        ],),
+      )
     ],),);
   }
 

ae930ad7389c6df83505074b8d61938e277c854e
Jesus bossa <x@jesusbossa.com>
2020-01-16 16:35:06 -0500

    [ADD] ADMOB Integration

--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -5,6 +5,7 @@ import 'package:trips/User/model/user.dart';
 import 'package:trips/widgets/button_green.dart';
 import 'package:trips/widgets/gradientBack.dart';
 import 'package:trips/widgets/navigation_bar.dart';
+import 'package:firebase_admob/firebase_admob.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -28,6 +29,11 @@ class _SignInScreen extends State<SignInScreen> {
   }
 
   Widget _handleCurrentSession(){
+
+    FirebaseAdMob.instance.initialize(appId: 'ca-app-pub-1183317843441765~1799465313').then((response) {
+      myBanner..load()..show(horizontalCenterOffset: 2);
+    });
+
     return StreamBuilder(
       stream: userBlock.authStatus,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -80,3 +86,25 @@ class _SignInScreen extends State<SignInScreen> {
   }
   
 }
+
+MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+  keywords: <String>['games', 'apps'],
+  contentUrl: 'https://flutter.io',
+  // birthday: DateTime.now(),
+  childDirected: false,
+  // designedForFamilies: false,
+  // gender: MobileAdGender.male, // or MobileAdGender.female, MobileAdGender.unknown
+  testDevices: <String>[], // Android emulators are considered test devices
+);
+
+BannerAd myBanner = BannerAd(
+  // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+  // https://developers.google.com/admob/android/test-ads
+  // https://developers.google.com/admob/ios/test-ads
+  adUnitId: BannerAd.testAdUnitId,
+  size: AdSize.smartBanner,
+  targetingInfo: targetingInfo,
+  listener: (MobileAdEvent event) {
+    print("BannerAd event is $event");
+  },
+);
